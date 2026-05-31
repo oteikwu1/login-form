7fa2ff8787aaaeff43d8cc63640899dd2f88d029
oteikwu1 <oteikwuon@gmail.com>
2026-05-31 11:23:57 +0100

    add all the neccessary JS variable and endPoint

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const passwordInput = document.getElementById('pass-input');
+const repeatPass = document.getElementById('repeat-pass');
+const checkbox = document.getElementById('checkbox');
+const signUpBtn = document.getElementById('sign-up-btn');
+
+
+async function handleFormRegister(fromData) {
+  const endPoint = 'https://reqres.in/api/register';
+  try {
+    const response = await fetch(endPoint, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        name: FormData.name,
+        email: FormData.email,
+        password: FormData.password,
+        agreedTerms: FormData.agreedTerms,
+      }),
+    });
+
+    if (response.ok) {
+      console.log('registration successfully', FormData.token);
+    } else {
+         console.log('registration failed', FormData.token);
+    }
+  } catch (error) {
+    throw new Error(`Invalid enter {error.status}`);
+  }
+};
 
